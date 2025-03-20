f338fc9eeef773ceba429512c7eb9558aafed73c
Andrew Israel <andrew@propelauth.com>
2025-03-20 12:23:43 -0700

    Add warning for accidentally using this repo

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a placeholder repo for the PropelAuth CLI.
+
+You are likely looking for [this repo](https://github.com/PropelAuth/cli) instead.
---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+console.warn(
+  "You likely wanted to use the package named @propelauth/cli instead. Please use `npx @propelauth/cli` or install @propelauth/cli globally.",
+);
+process.exit(1);
